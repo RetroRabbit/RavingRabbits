4373d1a6e4a252f1f8e06e8c04372d3132e5b187
Arsalan <aahmed@retrorabbit.co.za>
2018-01-07 02:51:43 +0200

    modified app to link routes and completed registration layout

--- ChatterBox/try-app/src/container/app/index.js
+++ ChatterBox/try-app/src/container/app/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import LoginPage from '../LoginPage';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Register from '../Registration/register.js'
+import StepTwo from '../setProfile/stepTwo.js'
+import StepLast from '../setProfile/stepLast.js'
 
 
 
@@ -9,7 +12,10 @@ const App = () => (
   <MuiThemeProvider>
   <div>
     <main>
-      <Route exact path="/" component={LoginPage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/step-two" component={StepTwo} />
+      <Route exact path="/last-step" component={StepLast} />
     </main>
   </div>
   </MuiThemeProvider>
