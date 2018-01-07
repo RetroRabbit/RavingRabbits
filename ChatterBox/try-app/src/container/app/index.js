2c8bf275962fef00c7215ca4411f475caca263ab
Arsalan <aahmed@retrorabbit.co.za>
2018-01-07 03:04:22 +0200

    completed CSS, Register layout and login layout

--- ChatterBox/try-app/src/container/app/index.js
+++ ChatterBox/try-app/src/container/app/index.js
@@ -3,8 +3,6 @@ import { Route, Link } from 'react-router-dom';
 import LoginPage from '../LoginPage';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Register from '../Registration/register.js'
-import StepTwo from '../setProfile/stepTwo.js'
-import StepLast from '../setProfile/stepLast.js'
 
 
 
@@ -14,8 +12,7 @@ const App = () => (
     <main>
       <Route exact path="/login" component={LoginPage} />
       <Route exact path="/register" component={Register} />
-      <Route exact path="/step-two" component={StepTwo} />
-      <Route exact path="/last-step" component={StepLast} />
+
     </main>
   </div>
   </MuiThemeProvider>
