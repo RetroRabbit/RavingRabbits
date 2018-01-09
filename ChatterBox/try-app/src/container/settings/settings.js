67937db69d8c1633124441be680329c8f1c75802
Lunga Nkosi <lnkosi@retrorabbit.co.za>
2018-01-09 08:34:38 +0200

    Fixed Setting page

--- ChatterBox/try-app/src/container/settings/settings.js
+++ ChatterBox/try-app/src/container/settings/settings.js
@@ -1,41 +1,53 @@
-import React, { Component } from 'react'
-import { TextField, RaisedButton } from 'material-ui'
+import React, { Component } from 'react';
+import { TextField, RaisedButton } from 'material-ui';
 import { colors } from 'material-ui/styles';
 import { white } from 'material-ui/styles/colors';
-import { push } from 'react-router-redux'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import Header from '../Header'
+import { push } from 'react-router-redux';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Header from '../Header';
 
-import "./settings.css";
+import './settings.css';
 const settings = props => (
     <div className="background">
-    <header className="Rectangle-3">
-        <Header/>
-    </header>
+        <header className="Rectangle-3">
+            <Header />
+        </header>
+
         <div className="Settingss">
-        <div className="ovall"></div>
-        
-        <h1>Addie Hogan <i className="fa fa-pencil" aria-hidden="true"></i></h1>
-        <p>addiehogan@gmail&#46;com <i className="fa fa-pencil" id= "email" aria-hidden="true"></i></p>
-        <form >
-          <div>
-            <br/><RaisedButton label = "Done" className="button" id = "done"/>
-          </div>
-        </form>
-      </div>
+            <div className="ovall" />
+
+            <h1>
+                Addie Hogan <i className="fa fa-pencil" aria-hidden="true" />
+            </h1>
+            <h3>
+                addiehogan@gmail&#46;com{' '}
+                <i className="fa fa-pencil" id="email" aria-hidden="true" />
+            </h3>
+            <form>
+                <div>
+                    <br />
+                    <RaisedButton
+                        onClick={() => props.changePage()}
+                        label="Done"
+                        className="button"
+                        id="done"
+                        labelColor="color:#fff;"
+                    />
+                </div>
+            </form>
+        </div>
     </div>
-)
+);
 
-const mapStateToProps = state => ({
+const mapStateToProps = state => ({});
 
-})
+const mapDispatchToProps = dispatch =>
+    bindActionCreators(
+        {
+            changePage: () => push('/chatFrom')
+        },
+        dispatch
+    );
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  changePage: () => push('/login')
-}, dispatch)
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(settings)
+export default connect(mapStateToProps, mapDispatchToProps)(settings);
