588e4ea92a67fc9fbc485bfb0b7974afe1e05601
Johan Laubscher <jlaubscher@retrorabbit.co.za>
2018-01-09 10:52:58 +0200

    Changed Header css and update the way it is displayed for mobile devices

--- ChatterBox/try-app/src/container/Header/index.js
+++ ChatterBox/try-app/src/container/Header/index.js
@@ -4,38 +4,91 @@ import { TextField, RaisedButton } from 'material-ui'
 import AppBar from 'material-ui/AppBar'
 import './header.css'
 
-const Header = props => (
-  <div class="header">
-    <div class="left-header-buttons">
-      <div class="dropdown">
-        <button class="dropdown-button btn header-button"><span class="header-button-label">NEW CHAT</span></button>
-        <div class="dropdown-content">
-          <TextField
-            hintText="Friend's Email"
-          />
-        </div>
-      </div>
-      <div class="dropdown">
-        <button class="dropdown-button btn header-button"><span class="header-button-label">NEW GROUP</span></button>
-        <div class="dropdown-content">
-          <TextField
-            hintText="Group Name"
-          />
-        </div>
-      </div>
-      <div class="pull-right">
-        <div class="account-dropdown">
-          <span class="account-dropdown-button user-name">Raving Rabbits</span>
-          <div class="account-dropdown-content">
-            <a href="/profile-settings">Settings</a>
-            <a href="/login">Log Out</a>
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  };
+
+  render() {
+    const { width } = this.state;
+    const isMobile = width <= 764;
+
+    if (isMobile) {
+      return (
+        <div class="header">
+          <div class="left-header-buttons">
+            <div class="dropdown">
+              <button class="dropdown-button btn header-button"><span class="header-button-label">NEW CHAT</span></button>
+              <div class="dropdown-content">
+                <TextField
+                  hintText="Friend's Email"
+                />
+              </div>
+            </div>
+            <div class="dropdown">
+              <button class="dropdown-button btn header-button"><span class="header-button-label">NEW GROUP</span></button>
+              <div class="dropdown-content">
+                <TextField
+                  hintText="Group Name"
+                />
+              </div>
+            </div>
+            <div class="pull-right">
+              <span class="account-dropdown-button user-name"><span class="glyphicon glyphicon-chevron-right"></span></span>
+            </div>
           </div>
         </div>
-        <img id="profile-img" class="profile-img" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-        <img id="profile-img" class="profile-img" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-      </div>
-    </div>
-  </div>
-)
-
+      );
+    } else {
+      return (
+        <div class="header">
+          <div class="left-header-buttons">
+            <div class="dropdown">
+              <button class="dropdown-button btn header-button"><span class="header-button-label">NEW CHAT</span></button>
+              <div class="dropdown-content">
+                <TextField
+                  hintText="Friend's Email"
+                />
+              </div>
+            </div>
+            <div class="dropdown">
+              <button class="dropdown-button btn header-button"><span class="header-button-label">NEW GROUP</span></button>
+              <div class="dropdown-content">
+                <TextField
+                  hintText="Group Name"
+                />
+              </div>
+            </div>
+            <div class="pull-right">
+              <div class="account-dropdown">
+                <span class="account-dropdown-button user-name">Raving Rabbits</span>
+                <div class="account-dropdown-content">
+                  <a href="/settings">Settings</a>
+                  <a href="/login">Log Out</a>
+                </div>
+              </div>
+              <img id="profile-img" class="profile-img" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+              <img id="profile-img" class="profile-img" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+}
 export default Header;
