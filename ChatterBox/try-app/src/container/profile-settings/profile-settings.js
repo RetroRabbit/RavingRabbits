a9409ad03f172275be25fcff4ea371ed251ff70b
Johan Laubscher <jlaubscher@retrorabbit.co.za>
2018-01-10 16:33:49 +0200

    Added reducer to update names

--- ChatterBox/try-app/src/container/profile-settings/profile-settings.js
+++ ChatterBox/try-app/src/container/profile-settings/profile-settings.js
@@ -6,56 +6,62 @@ import { push } from 'react-router-redux';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Header from '../Header';
-
-
+import { updateName } from '../../helpers/reducerLogin';
 import './profile-settings.css';
-const settings = props => (
-    <div className="whiteBackground">
-        <header className="Rectangle-3">
-            <Header />
-        </header>
 
-        <div class="row">
-            <div class="topSection">
-                <img src={require('./Oval.png')} class="img-circle center-block"/>
-                <img src={require('./mypic.JPG')} class="profile img-circle"/>
-                <div id="example-one" contenteditable="true" class="mylink">
-                    
-                        <h1>Arsalan Ahmed   
-                        </h1>
-                        
-                        <h3>ash@gmail&#46;com{' '}
-                        </h3>
+class settings extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    updateNames(email, userName) {
+        this.props.updateName(email, userName);
+    }
+
+    render() {
+        return (
+            <div className="whiteBackground">
+                <header className="Rectangle-3">
+                    <Header />
+                </header>
+                <div class="row">
+                    <div class="topSection">
+                        <img src={require('./Oval.png')} class="img-circle center-block" />
+                        <img src={require('./mypic.JPG')} class="profile img-circle" />
+                        <div id="example-one" contenteditable="true" class="mylink">
+                            <h1>{this.props.userName}</h1>
+                            <h3>{this.props.email}&nbsp;</h3>
+                        </div>
+                        <h1><i className="fa fa-pencil" aria-hidden="true" /></h1>
+                        <h3><i className="fa fa-pencil" id="email" aria-hidden="true" /></h3>
+                    </div>
+                    <form>
+                        <div>
+                            <br />
+                            <RaisedButton
+                                onClick={() => this.updateNames(this.props.email, this.props.userName)}
+                                label="Done"
+                                className="button"
+                                id="done"
+                            />
+                        </div>
+                    </form>
                 </div>
-                <h1><i className="fa fa-pencil" aria-hidden="true" /></h1>
-                <h3><i className="fa fa-pencil" id="email" aria-hidden="true" /></h3>
             </div>
-            <form>
-                <div>
-                    <br />
-                    <RaisedButton
-                        onClick={() => props.changePage()}
-                        label="Done"
-                        className="button"
-                        id="done"
-                        labelColor="color:#fff;"
-                    />
-                </div>
-            </form>
-          
-        </div>
-    </div>
-);
+        );
+    }
+};
 
-const mapStateToProps = state => ({});
+const mapStateToProps = ({ loginReducer }) => {
+    return {
+        userName: loginReducer.userName,
+        email: loginReducer.email
+    };
+};
 
-const mapDispatchToProps = dispatch =>
-    bindActionCreators(
-        {
-            changePage: () => push('/chatareamessages')
-            
-        },
-        dispatch
-    );
+const mapDispatchToProps = dispatch => bindActionCreators({
+    changePage: () => push('/chatareamessages'),
+    updateName
+}, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(settings);
--- ChatterBox/try-app/src/helpers/reducerLogin.js
+++ ChatterBox/try-app/src/helpers/reducerLogin.js
@@ -1,19 +1,30 @@
 import { push } from "react-router-redux";
+const initialState = {
+  
+    isLoginSuccess: false,
+    isLoginPending: false,
+    loginError: null,
+    userName: 'Raving Rabbits',
+    email: 'RavingRabbits@gmail.com'
+  
+}
 
 const SET_LOGIN_PENDING = 'SET_LOGIN_PENDING';
 const SET_LOGIN_SUCCESS = 'SET_LOGIN_SUCCESS';
 const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR';
+const UPDATE_NAME = 'UPDATE_NAME';
 
 export function login(email, password) {
   return dispatch => {
     dispatch(setLoginPending(true));
     dispatch(setLoginSuccess(false));
     dispatch(setLoginError(null));
+    dispatch(updateName(null));
 
     callLoginApi(email, password, error => {
       dispatch(setLoginPending(false));
       if (!error) {
-        dispatch(setLoginSuccess(true));
+        dispatch(setLoginSuccess(true,email));
       } else {
         dispatch(setLoginError(error));
       }
@@ -28,10 +39,17 @@ function setLoginPending(isLoginPending) {
   };
 }
 
-function setLoginSuccess(isLoginSuccess) {
+function setLoginSuccess(isLoginSuccess,email) {
   return {
     type: SET_LOGIN_SUCCESS,
-    isLoginSuccess
+    isLoginSuccess,email
+  };
+}
+
+export function updateName(email,userName) {
+  return {
+    type: UPDATE_NAME,
+    email, userName
   };
 }
 
@@ -53,12 +71,7 @@ function callLoginApi(email, password, callback) {
   }, 1000);
 }
 
-export function loginReducer(state = {
-  isLoginSuccess: false,
-  isLoginPending: false,
-  loginError: null,
-  userName: 'Raving Rabbits'
-}, action) {
+export function loginReducer(state = initialState, action) {
   switch (action.type) {
     case SET_LOGIN_PENDING:
       return Object.assign({}, state, {
@@ -68,12 +81,19 @@ export function loginReducer(state = {
     case SET_LOGIN_SUCCESS:
       return Object.assign({}, state, {
         isLoginSuccess: action.isLoginSuccess,
-        userName: 'ash@gmail.com'
+        userName: 'Arsalan Ahmed',
+        email: action.email
       });
 
     case SET_LOGIN_ERROR:
       return Object.assign({}, state, {
         loginError: action.loginError
+      });
+
+      case UPDATE_NAME:
+      return Object.assign({}, state, {
+        userName: action.userName,
+        email: action.email
       });
 
     default:
