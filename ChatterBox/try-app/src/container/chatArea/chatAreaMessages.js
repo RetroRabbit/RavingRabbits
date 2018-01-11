a25d64b7d3f7406d684acb6493d5ad5c256c677d
Lawrence Mafisa <lmafisa@retrorabbit.co.za>
2018-01-11 11:42:36 +0200

    styled text bubble

--- ChatterBox/try-app/src/container/chatArea/chatAreaMessages.js
+++ ChatterBox/try-app/src/container/chatArea/chatAreaMessages.js
@@ -18,7 +18,7 @@ function formatAMPM(date) {
   var minutes = date.getMinutes();
   var ampm = hours >= 12 ? 'PM' : 'AM';
   hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
+  hours = hours ? hours : 12;// the hour '0' should be '12'
   minutes = minutes < 10 ? '0'+minutes : minutes;
   var strTime = hours + ':' + minutes + ' ' + ampm;
   return strTime;
@@ -30,24 +30,25 @@ function insertChat(who, text, time = 0){
  
   if (who === "me"){
       
-      control = '<li style="width:50%">' +
-                      '<div class="msj macro">' +
-                      '<div class="avatar"><img className="img-circle" style="width:20%;"  /></div>' +
-                          '<div class="text text-l">' +
+      control = '<li>' +
+                      '<div class="p1">' +
+                            '<div class="message">' +
                               '<p>'+ text +'</p>' + 
-                              '<p><small><font color="white">'+date+'</font></small></p>' +
-                          '</div>' +
+                            '</div>' +
+                                '<div>' +
+                              '<p class="time">'+date+'</p>' +
+                                '</div>' +
                       '</div>' +
-                  '</li>';                    
+                '</li>';                    
   }else{
       control = '<li style="width:90%;">' +
-                      '<div class="msj-rta macro">' +
+                  '<div class="msj-rta macro">' +
                           '<div class="text text-r">' +
                               '<p>'+text+'</p>' +
                                 '<div>' +
                               '<p><small><font color="white">'+date+'</font></small></p>' +
-                                  '</div>' +
                           '</div>' +
+                    '</div>' +
                       '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:20%;" /></div>' +                                
                 '</li>';
               }
@@ -81,7 +82,6 @@ function insertChat(who, text, time = 0){
 class chatArea extends Component{
   constructor(props) {
     super(props);
- 
     this.toggle = this.toggle.bind(this);
     this.state = {
       isOpen: false
@@ -111,7 +111,7 @@ class chatArea extends Component{
       <form>
           <div className="addMessage">
                <FloatingActionButton  disabled={false} className="add">
-
+             
                <ContentAdd />
               </FloatingActionButton>
           </div>
